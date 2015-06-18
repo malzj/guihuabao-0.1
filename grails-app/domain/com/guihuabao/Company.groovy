9404be7bcd0ec0d37b338a6c127b785ea9af0091
609658960@qq.com <609658960@qq.com>
2015-06-18 11:49:54 +0800

    公司添加字段

--- grails-app/domain/com/guihuabao/Company.groovy
+++ grails-app/domain/com/guihuabao/Company.groovy
@@ -2,6 +2,18 @@ package com.guihuabao
 
 class Company {
     String companyname
+    String companyAbbrev
+    String contactName
+    String sex
+    String job
+    String companyPhone
+    String telephone
+    String email
+    String fax
+    String companyType
+    Date regtime
+    String postalcode
+    String website
     String remark
     Date dateUse
     Date dateCreat
@@ -13,5 +25,17 @@ class Company {
         remark(nullable: true)
         address(nullable: true)
         phone(nullable: true)
+        companyAbbrev(nullable: true)
+        contactName(nullable: true)
+        sex(nullable: true)
+        job(nullable: true)
+        companyPhone(nullable: true)
+        telephone(nullable: true)
+        email(nullable: true)
+        fax(nullable: true)
+        companyType(nullable: true)
+        regtime(nullable: true)
+        postalcode(nullable: true)
+        website(nullable: true)
     }
 }
